87f44bb65d4afeda4703287dfc34a00fc4bd074d
Israel Canul <icanul@iran-imac.royalresorts.local>
2017-02-13 15:58:11 -0500

    config file added

---
+++ app/src/main/java/restauran/lebasillic/menurestaurant/configuracion.java
@@ -0,0 +1,11 @@
+package restauran.lebasillic.menurestaurant;
+
+/**
+ * Created by icanul on 2/13/17.
+ */
+
+public final class configuracion {
+    static final boolean footerFixed = true;
+    static final boolean headerFixed = true;
+
+}
